58a15dcd8377e1e9c3c4351197ed68f1bdfb5602
Savon Earle <searle@nyc.yearup.org>
2018-11-08 16:15:15 -0500

    YAY

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,2 +1,13 @@
 //This comes from data.js
 let gifs = window.data.data;
+
+function randomGif(){
+  let meow = document.getElementById('CATS')
+  let randomGIFs = Math.floor(Math.random()*gifs.length);
+  meow.innerHTML = gifs[randomGIFs];
+  meow.src=gifs[randomGIFs].images.original.url;
+  console.log (gifs[randomGIFs]);
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', randomGif);
